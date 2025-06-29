f4d0f2c01b5e937fd8214dc17854b442db1de3c2
adityar947 <adityar947@gmail.com>
2025-06-29 12:53:39 +0530

    first commit

---
+++ 3_variables/variables.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "golang"
+	// var name = "golang"
+	// var isAdult = true
+	// var age int = 25
+	// var age = 25
+	// fmt.Println(name)
+
+	// shorthand syntax
+
+	// name := "Aditya"
+
+	// var price float32 = 50.5
+	// var price = 50.5
+	price := 50.5
+
+	fmt.Println(price)
+
+	// fmt.Println()
+}
---
+++ 4_constants/constants.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+	// var name string = "golang"
+
+	const (
+		port = 5000
+		host = "localhost"
+	)
+
+	fmt.Println(port, host)
+
+}
